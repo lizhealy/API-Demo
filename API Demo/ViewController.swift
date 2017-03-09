8b904cd61b8939c7c9efe3b663a018681da387cb
Liz Healy <lizhealy@Lizs-MacBook-Pro.local>
2017-03-09 10:21:28 -0500

    Initial Commit

--- API Demo/ViewController.swift
+++ API Demo/ViewController.swift
@@ -13,6 +13,48 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a1a51f09406715c9d14a9dcc9cebaa33")!
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            if error != nil {
+                print(error)
+            } else {
+                
+                if let urlContent = data {
+                    
+                    do {
+                    
+                     let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                    
+                        print(jsonResult)
+                        
+                        print(jsonResult["name"])
+                        
+                        if let description = ((jsonResult["weather"] as? NSArray)?[0] as? NSDictionary)?["description"] as? String {
+                            
+                            print(description)
+                            
+                        }
+                        
+                        
+                        
+                        
+                    } catch {
+                        
+                        print("JSON Processing Failed")
+                        
+                    }
+                }
+                
+            }
+            
+        }
+        
+        task.resume()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
